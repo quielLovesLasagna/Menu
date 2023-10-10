a4978c23eb07ad4a94f2a59b42a372e740c62094
Exequiel Arco <exequielarco23@gmail.com>
2023-10-10 19:48:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const menu = [
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
-    price: 15.99,
+    price: 100,
     img: "./images/item-1.jpeg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
@@ -13,7 +13,7 @@ const menu = [
     id: 2,
     title: "diner double",
     category: "lunch",
-    price: 13.99,
+    price: 120,
     img: "./images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
@@ -21,7 +21,7 @@ const menu = [
     id: 3,
     title: "godzilla milkshake",
     category: "shakes",
-    price: 6.99,
+    price: 80,
     img: "./images/item-3.jpeg",
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
@@ -29,7 +29,7 @@ const menu = [
     id: 4,
     title: "country delight",
     category: "breakfast",
-    price: 20.99,
+    price: 110,
     img: "./images/item-4.jpeg",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
@@ -37,7 +37,7 @@ const menu = [
     id: 5,
     title: "egg attack",
     category: "lunch",
-    price: 22.99,
+    price: 70,
     img: "./images/item-5.jpeg",
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
@@ -45,7 +45,7 @@ const menu = [
     id: 6,
     title: "oreo dream",
     category: "shakes",
-    price: 18.99,
+    price: 85,
     img: "./images/item-6.jpeg",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
@@ -53,7 +53,7 @@ const menu = [
     id: 7,
     title: "bacon overflow",
     category: "breakfast",
-    price: 8.99,
+    price: 90,
     img: "./images/item-7.jpeg",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
@@ -61,7 +61,7 @@ const menu = [
     id: 8,
     title: "american classic",
     category: "lunch",
-    price: 12.99,
+    price: 120,
     img: "./images/item-8.jpeg",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
@@ -69,7 +69,7 @@ const menu = [
     id: 9,
     title: "quarantine buddy",
     category: "shakes",
-    price: 16.99,
+    price: 140,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -77,7 +77,7 @@ const menu = [
     id: 10,
     title: "steak dinner",
     category: "dinner",
-    price: 39.99,
+    price: 180,
     img: "./images/item-10.jpeg",
     desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero vel ab necessitatibus unde quasi excepturi dolorum corporis est exercitationem cum!`,
   },
@@ -97,7 +97,7 @@ const displayMenuItems = function (menuItems) {
       <div class="item-info">
         <header>
           <h4>${item.title}</h4>
-          <h4 class="price">$${item.price}</h4>
+          <h4 class="price">₱${item.price}</h4>
         </header>
         <p class="item-text">
           ${item.desc}
